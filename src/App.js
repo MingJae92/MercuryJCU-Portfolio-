57074b64c2de673b24605a34e22a6c3c09158051
Ming Chi <wong_mingchi@yahoo.co.uk>
2022-05-31 02:44:43 +0100

    Navbar Drawer added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Sidebar from "./Sidebar"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-        
-
         <h1>Mercury JC!!!</h1>
+        <Sidebar/>
       </header>
     </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,48 +1,126 @@
-import Button from '@mui/material/Button';
-import './Shop.css'
-import {
-  List,
-  ListItemText,
-  Drawer
-} from "@material-ui/core";
-
 import React from 'react'
-import Homepage from './routes/Homapage';
-import MyWork from './routes/MyWork';
-import Commissions from './Commissions';
-import Shop from './routes/Shop';
-const listItems = [
-    {
-        listIcons: <Homepage/>,
-        listText: "Home"
-    },
-    {
-        listIcons: <MyWork/>,
-        listText: "MyWork"
-    },
-    {
-        listIcons: <Commissions/>,
-        listText: "Commissions"
-    },
-    {
-        listIcons: <Shop/>,
-        listText: "Shop"
-    },
-    {
-        listIcons: <AboutMe/>,
-        listText: "AboutMe"
-    },
-    {
-        listIcons: <ContactMe/>,
-        listText: "ContactMe"
-    },
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { BrowserRouter as Router,
+    Route,
+    Routes,
+    Link,
+    useLocation, } from 'react-router';
 
-]
+// import Homepage from './routes/Homapage';
+// import MyWork from './routes/MyWork';
+// import Commissions from './Commissions';
+// import Shop from './routes/Shop';
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
+
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
+
 const Sidebar = () => {
+
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+  
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleDrawerClose = () => {
+      setOpen(false);
+    };
   return (
-    <div>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+           Menu
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              {/* <ListItemButton> */}
+                <ListItemIcon>
+                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              {/* </ListItemButton> */}
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        
+      </Drawer>
       
-    </div>
+    </Box>
   )
 }
 
